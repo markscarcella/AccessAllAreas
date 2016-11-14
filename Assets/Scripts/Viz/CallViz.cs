f0322f26edddf5244054d28ecb139e5fed3b59f0
DESKTOP-7LG739E\Destined <j.oliver@destined.com>
2016-11-15 10:39:11 +1100

    Viz

---
+++ Assets/Scripts/Viz/CallViz.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallViz : MonoBehaviour {
+
+	public string type = "circles";
+
+	private float timeToNote = 0.5f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		RandomNoteGenerator ();
+	}
+
+	void RandomNoteGenerator()
+	{
+		timeToNote -= Time.deltaTime;
+
+		if (timeToNote < 0.0f)
+		{
+			AddNote (Random.Range (0, 10), Random.Range (0, 100), Random.Range (0, 255));
+			if (Random.Range (0, 10) < 5)
+			{
+				AddNote (Random.Range (0, 10), Random.Range (0, 100), Random.Range (0, 255));
+				if (Random.Range (0, 10) < 5)
+				{
+					AddNote (Random.Range (0, 10), Random.Range (0, 100), Random.Range (0, 255));
+				}
+			}
+			timeToNote = Random.Range (0.0f, 1.0f);
+		}
+	}
+
+	public void AddNote(int instrument, int note, int velocity)
+	{
+		if (type == "circles")
+		{
+
+			GetComponent<VizCircles> ().VizNote (instrument, note, velocity);
+		}
+	}
+}
---
+++ Assets/Scripts/Viz/CircleController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleController : MonoBehaviour {
+
+	// Use this for initialization
+
+	private SpriteRenderer mySpriteRender;
+
+	void Start () {
+		//Destroy(this, 2.0f);
+		mySpriteRender = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ShrinkCircle ();
+		MakeTransparent ();
+	}
+
+	void ShrinkCircle()
+	{
+		transform.localScale -= new Vector3 (Time.deltaTime * 0.2f, Time.deltaTime * 0.2f, Time.deltaTime * 0.2f);
+
+		if (transform.localScale.x < 0.0)
+		{
+			Destroy (gameObject);
+		}
+	}
+
+	void MakeTransparent()
+	{
+
+		mySpriteRender.material.color = new Color(mySpriteRender.material.color.r,mySpriteRender.material.color.g,mySpriteRender.material.color.b,mySpriteRender.material.color.a - Time.deltaTime * 0.2f);
+	}
+}
