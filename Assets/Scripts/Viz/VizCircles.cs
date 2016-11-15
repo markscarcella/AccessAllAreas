15d1b4819d87fe1d7e0f0485482cf87480095e43
DESKTOP-7LG739E\Destined <j.oliver@destined.com>
2016-11-15 12:10:06 +1100

    no message

--- Assets/Scripts/Viz/VizCircles.cs
+++ Assets/Scripts/Viz/VizCircles.cs
@@ -28,13 +28,29 @@ public class VizCircles : MonoBehaviour {
 			int index = Random.Range (0, myCircles.Length - 1);
 			float tempScale = Random.Range (0.8f, 1.0f);
 			myCircles[index].transform.localScale = new Vector3 (tempScale, tempScale, tempScale);
-			myCircles[index].GetComponent<SpriteRenderer> ().material.color = new Color (1, 1, 1, 1);
+			if(instrument == 0)
+				myCircles[index].GetComponent<SpriteRenderer> ().material.color = new Color (255.0f/255.0f, 204.0f/255.0f, 1, 1);
+			if(instrument == 1)
+				myCircles[index].GetComponent<SpriteRenderer> ().material.color = new Color (217.0f/255.0f, 94.0f/255.0f, 64.0f/255.0f, 1);
+			if(instrument == 2)
+				myCircles[index].GetComponent<SpriteRenderer> ().material.color = new Color (217.0f/255.0f, 94.0f/255.0f, 64.0f/255.0f, 1);
+			else
+				myCircles[index].GetComponent<SpriteRenderer> ().material.color = new Color (1, 1, 1, 1);
+				
 		} 
 		else
 		{
 			GameObject myObj = Instantiate (spriteCircle, new Vector3 (Random.Range (-8.0f, 8.0f), Random.Range (-4.0f, 4.0f), 0), Quaternion.identity) as GameObject;
 			float tempScale = Random.Range (0.5f, 1.0f);
 			myObj.transform.localScale = new Vector3 (tempScale, tempScale, tempScale);
+			if(instrument == 0)
+				myObj.GetComponent<SpriteRenderer> ().material.color = new Color (255.0f/255.0f, 204.0f/255.0f, 1, 1);
+			if(instrument == 1)
+				myObj.GetComponent<SpriteRenderer> ().material.color = new Color (217.0f/255.0f, 94.0f/255.0f, 64.0f/255.0f, 1);
+			if(instrument == 2)
+				myObj.GetComponent<SpriteRenderer> ().material.color = new Color (217.0f/255.0f, 94.0f/255.0f, 64.0f/255.0f, 1);
+			else
+				myObj.GetComponent<SpriteRenderer> ().material.color = new Color (1, 1, 1, 1);
 
 		}
 	}
