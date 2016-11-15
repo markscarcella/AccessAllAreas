5d8d0c29d3c93bbe734d7103df2f54b513a9f5a1
Curtis William Black <curtis.w.black@gmail.com>
2016-11-15 12:22:34 +1100

    deleted js

---
+++ Assets/Scripts/BackingBeat.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackingBeat : MonoBehaviour
+{
+    private BeatCounter beat;
+    private Band band;
+
+	void Start()
+    {
+        beat = GameObject.Find("BeatCounter").GetComponent<BeatCounter>();
+        band = GameObject.Find("Band").GetComponent<Band>();
+	}
+	
+	void Update()
+    {
+	    if(beat.Beat)
+        {
+            switch(beat.BeatCount % beat.BeatsPerBar)
+            {
+                case 0:
+                    band.AddMessage(new Message(InstrumentType.Drum, Note.C4, 255));
+                    break;
+                case 1:
+                    band.AddMessage(new Message(InstrumentType.Drum, Note.C4, 100));
+                    break;
+                case 2:
+                    band.AddMessage(new Message(InstrumentType.Drum, Note.C4, 180));
+                    break;
+                case 3:
+                    band.AddMessage(new Message(InstrumentType.Drum, Note.C4, 100));
+                    break;
+            }
+        }
+	}
+}
